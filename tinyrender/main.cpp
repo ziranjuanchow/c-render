619623f49871f4aa90daa3353d19965c593b90ff
Maxwell Chow <ziranjuanchow@gmail.com>
2022-01-12 17:40:23 +0800

    update

--- tinyrender/main.cpp
+++ tinyrender/main.cpp
@@ -1,56 +1,87 @@
 #include <vector>
 #include <cmath>
 #include "tgaimage.h"
+#include "model.h"
 #include "geometry.h"
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
-const TGAColor red = TGAColor(255, 0, 0, 255);
-const TGAColor green = TGAColor(0, 255, 0, 255);
-const int width = 200;
-const int height = 200;
+const TGAColor red   = TGAColor(255, 0,   0,   255);
+const TGAColor green = TGAColor(0,   255, 0,   255);
+Model *model = NULL;
+const int width  = 800;
+const int height = 800;
 
-void line(Vec2i p0, Vec2i p1, TGAImage& image, TGAColor color) {
+void line(Vec2i p0, Vec2i p1, TGAImage &image, TGAColor color) {
     bool steep = false;
-    if (std::abs(p0.x - p1.x) < std::abs(p0.y - p1.y)) {
+    if (std::abs(p0.x-p1.x)<std::abs(p0.y-p1.y)) {
         std::swap(p0.x, p0.y);
         std::swap(p1.x, p1.y);
         steep = true;
     }
-    if (p0.x > p1.x) {
+    if (p0.x>p1.x) {
         std::swap(p0, p1);
     }
 
-    for (int x = p0.x; x <= p1.x; x++) {
-        float t = (x - p0.x) / (float)(p1.x - p0.x);
-        int y = p0.y * (1. - t) + p1.y * t;
+    for (int x=p0.x; x<=p1.x; x++) {
+        float t = (x-p0.x)/(float)(p1.x-p0.x);
+        int y = p0.y*(1.-t) + p1.y*t;
         if (steep) {
             image.set(y, x, color);
-        }
-        else {
+        } else {
             image.set(x, y, color);
         }
     }
 }
 
-void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage& image, TGAColor color) {
-    line(t0, t1, image, color);
-    line(t1, t2, image, color);
-    line(t2, t0, image, color);
+void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color) {
+    if (t0.y==t1.y && t0.y==t2.y) return; // i dont care about degenerate triangles
+    if (t0.y>t1.y) std::swap(t0, t1);
+    if (t0.y>t2.y) std::swap(t0, t2);
+    if (t1.y>t2.y) std::swap(t1, t2);
+    int total_height = t2.y-t0.y;
+    for (int i=0; i<total_height; i++) {
+        bool second_half = i>t1.y-t0.y || t1.y==t0.y;
+        int segment_height = second_half ? t2.y-t1.y : t1.y-t0.y;
+        float alpha = (float)i/total_height;
+        float beta  = (float)(i-(second_half ? t1.y-t0.y : 0))/segment_height; // be careful: with above conditions no division by zero here
+        Vec2i A =               t0 + (t2-t0)*alpha;
+        Vec2i B = second_half ? t1 + (t2-t1)*beta : t0 + (t1-t0)*beta;
+        if (A.x>B.x) std::swap(A, B);
+        for (int j=A.x; j<=B.x; j++) {
+            image.set(j, t0.y+i, color); // attention, due to int casts t0.y+i != A.y
+        }
+    }
 }
 
 int main(int argc, char** argv) {
+    if (2==argc) {
+        model = new Model(argv[1]);
+    } else {
+        model = new Model("obj/african_head.obj");
+    }
+
     TGAImage image(width, height, TGAImage::RGB);
-
-    Vec2i t0[3] = { Vec2i(10, 70),   Vec2i(50, 160),  Vec2i(70, 80) };
-    Vec2i t1[3] = { Vec2i(180, 50),  Vec2i(150, 1),   Vec2i(70, 180) };
-    Vec2i t2[3] = { Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180) };
-
-    triangle(t0[0], t0[1], t0[2], image, red);
-    triangle(t1[0], t1[1], t1[2], image, white);
-    triangle(t2[0], t2[1], t2[2], image, green);
-
+    Vec3f light_dir(0,0,-1);
+    for (int i=0; i<model->nfaces(); i++) {
+        std::vector<int> face = model->face(i);
+        Vec2i screen_coords[3];
+        Vec3f world_coords[3];
+        for (int j=0; j<3; j++) {
+            Vec3f v = model->vert(face[j]);
+            screen_coords[j] = Vec2i((v.x+1.)*width/2., (v.y+1.)*height/2.);
+            world_coords[j]  = v;
+        }
+        Vec3f n = (world_coords[2]-world_coords[0])^(world_coords[1]-world_coords[0]);
+        n.normalize();
+        float intensity = n*light_dir;
+        if (intensity>0) {
+            triangle(screen_coords[0], screen_coords[1], screen_coords[2], image, TGAColor(intensity*255, intensity*255, intensity*255, 255));
+        }
+    }
 
     image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
     image.write_tga_file("output.tga");
+    delete model;
     return 0;
 }
+
--- tinyrender/model.h
+++ tinyrender/model.h
@@ -6,15 +6,15 @@
 
 class Model {
 private:
-	std::vector<Vec3f> verts_;
-	std::vector<std::vector<int> > faces_;
+    std::vector<Vec3f> verts_;
+    std::vector<std::vector<int> > faces_;
 public:
-	Model(const char* filename);
-	~Model();
-	int nverts();
-	int nfaces();
-	Vec3f vert(int i);
-	std::vector<int> face(int idx);
+    Model(const char *filename);
+    ~Model();
+    int nverts();
+    int nfaces();
+    Vec3f vert(int i);
+    std::vector<int> face(int idx);
 };
 
 #endif //__MODEL_H__
